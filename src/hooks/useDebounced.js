671df5b4fbc081226a16c5bb16b1eb8d286fc9d8
evertonbazu <84239016+evertonbazu@users.noreply.github.com>
2025-05-04 14:17:16 -0300

    Create useDebounced.js

---
+++ src/hooks/useDebounced.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export function useDebounced(callback, delay) {
+  const [debouncedCallback, setDebouncedCallback] = useState(() => callback);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedCallback(() => callback);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [callback, delay]);
+
+  return debouncedCallback;
+}
